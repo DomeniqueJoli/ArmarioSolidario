e60309fca9370ad2ffd643561cc2ab6678a2d77b
DomeniqueJoli <83772287+DomeniqueJoli@users.noreply.github.com>
2025-05-28 20:36:01 -0400

    Elementos tela de escolha

--- TelaEsolha.php
+++ TelaEsolha.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Armário Solidário</title>
-</head>
+    <img src="Images/logo.png" alt="">
+</head> 
 <body>
-    <img src="logo.png" alt="">
+    
 </body>
 </html>
