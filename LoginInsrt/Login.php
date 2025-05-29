f9a953a0b998c52aefc3b52d830a6983173429ee
DomeniqueJoli <83772287+DomeniqueJoli@users.noreply.github.com>
2025-05-29 15:10:28 -0400

    Atualizando login

--- LoginInsrt/Login.php
+++ LoginInsrt/Login.php
@@ -12,18 +12,23 @@
 <div class="container">
     <div class="quadrado fundo"></div>
     <div class="quadrado frente"></div>
+    <img src="../images/logo.png" class="logo" alt="" class="logo">
 
-    <img src="Images/logo.pnp" alt="" class="logo">
+    <div class="form">
+    <label for="" class="tt"><b>Usuário:</b></label>
+    <br>
+    <input class="ent" type="text">
+    <br>
+
+    <label for="" class="tt"><b>Senha:</b></label>
+    <br>
+    <input class="ent" type="text">
+    <a class="Sen" href="../RecuperarSenha/RecupSenha">Esqueci minha senha</a>
+<br>
+    <button class="bt" >Entrar</button>
+    </div>
+
 </div>
-
-<label for="">Usuário</label>
-    <input type="text">
-
-    <label for="">Senha</label>
-    <input type="text">
-
-    <button>Esqueci minha senha</button>
-    <button>Entrar</button>
 
 </body>
 </html>
