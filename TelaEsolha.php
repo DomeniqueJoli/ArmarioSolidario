6a5b9d40a2c067fc4e162db9d5e5c03ad13dd0e0
DomeniqueJoli <83772287+DomeniqueJoli@users.noreply.github.com>
2025-05-29 11:25:51 -0400

    25-05-2025

--- TelaEsolha.php
+++ TelaEsolha.php
@@ -6,15 +6,16 @@
     <title>Armário Solidário</title>
     <link rel="stylesheet" href="styleTelaEscolha.css">
     <link rel="icon" href="Images/Captura_de_tela_2025-05-28_202248-removebg-preview (1).png" type="image/png">
-    <img src="Images/logo.png" alt=""> 
-    <h1>Armário Solidário</h1>
+
 </head> 
 
 <body>
+<img src="Images/logo.png" alt=""> 
+<h1>Armário Solidário</h1>
 
 <div style="text-align: center;">
-    <button><b>Doador</b></button>
-    <button><b>Instituição</b></button>
+    <button>Doador</button>
+    <button>Instituição</button>
 </div>
 
 </body>
