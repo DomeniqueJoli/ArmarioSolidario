a9d7ca3bab9970be8a8bbf4e5ebaa16e8e1fe761
DomeniqueJoli <83772287+DomeniqueJoli@users.noreply.github.com>
2025-05-28 21:10:18 -0400

    Ajustando logo

--- TelaEsolha.php
+++ TelaEsolha.php
@@ -6,9 +6,10 @@
     <title>Armário Solidário</title>
     <link rel="stylesheet" href="styleTelaEscolha.css">
     <img src="Images/logo.png" alt="">
+    <h1>Armário Solidário</h1>
+
 </head> 
 <body>
-    <h1>Armário Solidário</h1>
     <button>Doador</button>
     <button>Instituição</button>
 </body>
