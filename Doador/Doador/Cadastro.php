8aa7e3e580a0f26c42b0ba81888131a072e452df
PumpItUp <rjoao@alunos.afya.com.br>
2025-05-30 19:59:35 -0400

    testing.Doador

--- Doador/Doador/Cadastro.php
+++ Doador/Doador/Cadastro.php
@@ -3,26 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cadastro do DOador</title>
 </head>
 <body>
     <form action="">
-        <div>
-        Nome completo: <input type="text" name="nomeDoador" required><br>
-        CPF: <input type="text" name="cpfDoador" required><br>
-        Descrição: <input type="text" name="bioDoador" required><br><
-        </div>
-    Data de Nascimento: <input type="date" name="dataDoador" required><br>
-    Contato: <input type="text" name="contatoDoador" required><br>
-    CEP: <input type="text" name="cepDoador" required><br>
-    Estado: <input type="text" name="estadoDoador" required><br>
-    Cidade: <input type="text" name="cidadeDoador" required><br>
+    Nome completo: <input type="text" name="nomeDoador" class="nomeD" required><br>
+    CPF: <input type="text" name="cpfDoador" class="cpfD" required><br>
+    Descrição: <input type="text" name="bioDoador" class="bioD" required><br>
+    Data de Nascimento: <input type="date" name="dataDoador" class="dataND" required><br>
+    Contato: <input type="text" name="contatoDoador" class="contatoD" required><br>
+    CEP: <input type="text" name="cepDoador" class="cepD" required><br>
+    Estado: <input type="text" name="estadoDoador" class="estadoD" required><br>
+    Cidade: <input type="text" name="cidadeDoador" class="cidadeD" required><br>
     Bairro: <input type="text" name="bairroDoador" required><br>
-    Rua: <input type="text" name="ruaDoador" required><br>
-    Número: <input type="number" name="numlocalDoador" required><br>
-    E-mail: <input type="email" name="emailDoador" required><br>
-    Senha: <input type="password" name="senhaDoador" required> Confirmar senha: <input type="password" name="senhaDoador" required><br><br>
-    
+    Rua: <input type="text" name="ruaDoador" class="ruaD" required><br>
+    Número: <input type="number" name="numlocalDoador" class="numD" required><br>
+    E-mail: <input type="email" name="emailDoador" class="emailD" required><br>
+    Senha: <input type="password" name="senhaDoador" class="senhaD"required> 
+    Confirmar senha: <input type="password" name="senhaDoador" class="confirm_senhaD" required><br><br>
+    dhsdi: <input type="button" class="jk">
     </form> 
     
     
