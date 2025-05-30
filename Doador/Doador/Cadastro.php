49b0748d6d390c874a8424072eb1c274a97dfb6e
PumpItUp <rjoao@alunos.afya.com.br>
2025-05-30 19:38:49 -0400

    Perfil Doador (beta)

---
+++ Doador/Doador/Cadastro.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <div>
+        Nome completo: <input type="text" name="nomeDoador" required><br>
+        CPF: <input type="text" name="cpfDoador" required><br>
+        Descrição: <input type="text" name="bioDoador" required><br><
+        </div>
+    Data de Nascimento: <input type="date" name="dataDoador" required><br>
+    Contato: <input type="text" name="contatoDoador" required><br>
+    CEP: <input type="text" name="cepDoador" required><br>
+    Estado: <input type="text" name="estadoDoador" required><br>
+    Cidade: <input type="text" name="cidadeDoador" required><br>
+    Bairro: <input type="text" name="bairroDoador" required><br>
+    Rua: <input type="text" name="ruaDoador" required><br>
+    Número: <input type="number" name="numlocalDoador" required><br>
+    E-mail: <input type="email" name="emailDoador" required><br>
+    Senha: <input type="password" name="senhaDoador" required> Confirmar senha: <input type="password" name="senhaDoador" required><br><br>
+    
+    </form>
+    
+    
+    
+</body>
+</html>
