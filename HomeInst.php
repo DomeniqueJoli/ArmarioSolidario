d1b075372cc844f3a9d35ae96c4141189f011164
DomeniqueJoli <83772287+DomeniqueJoli@users.noreply.github.com>
2025-05-30 21:32:05 -0400

    surtando com uma foto

--- HomeInst.php
+++ HomeInst.php
@@ -21,5 +21,22 @@
     <a href="Instituicao\Perfil.php">Perfil</a>
 </div>
 
+<div class="">
+<h6>Pequenos Gestos,</h6>
+<h1>Grandes Impactos</h1>
+</div>
+
+<div>
+<h3>Sobre a iniciativa Armário solidário</h3>
+<p>
+O Armário Solidário é uma iniciativa social que conecta pessoas dispostas a doar com instituições que acolhem quem mais precisa. <br>
+Acreditamos que roupas podem aquecer mais do que o corpo — elas transmitem dignidade, carinho e respeito. <br>
+
+Nosso objetivo é reaproveitar o que está parado nos armários e dar um novo destino a essas peças,
+levando conforto e autoestima a crianças, jovens, adultos e idosos em situação de vulnerabilidade.
+</p>
+
+</div>
+
 </body>
 </html>
