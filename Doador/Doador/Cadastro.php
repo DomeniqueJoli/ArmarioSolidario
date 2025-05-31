e7e93eb4a979dfadea834c4776b78db808ec5af6
PumpItUp <rjoao@alunos.afya.com.br>
2025-05-30 20:21:48 -0400

    arrumando Cadastro.php

--- Doador/Doador/Cadastro.php
+++ Doador/Doador/Cadastro.php
@@ -3,26 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleCadastro.css">
     <title>Cadastro do DOador</title>
 </head>
 <body>
-    <form action="">
-    Nome completo: <input type="text" name="nomeDoador" class="nomeD" required><br>
+  
+    Nome completo: <input type="text" name="nomeDoador" class="nomeD" ><br>
     CPF: <input type="text" name="cpfDoador" class="cpfD" required><br>
     Descrição: <input type="text" name="bioDoador" class="bioD" required><br>
-    Data de Nascimento: <input type="date" name="dataDoador" class="dataND" required><br>
+    Data de Nascimento: <input type="date" name="dataDoador" class="dataND" required>
     Contato: <input type="text" name="contatoDoador" class="contatoD" required><br>
     CEP: <input type="text" name="cepDoador" class="cepD" required><br>
-    Estado: <input type="text" name="estadoDoador" class="estadoD" required><br>
+
+    Estado: <input type="text" name="estadoDoador" class="estadoD" required>
     Cidade: <input type="text" name="cidadeDoador" class="cidadeD" required><br>
-    Bairro: <input type="text" name="bairroDoador" required><br>
-    Rua: <input type="text" name="ruaDoador" class="ruaD" required><br>
+    Bairro: <input type="text" name="bairroDoador" required>
+    Rua: <input type="text" name="ruaDoador" class="ruaD" required>
     Número: <input type="number" name="numlocalDoador" class="numD" required><br>
     E-mail: <input type="email" name="emailDoador" class="emailD" required><br>
     Senha: <input type="password" name="senhaDoador" class="senhaD"required> 
     Confirmar senha: <input type="password" name="senhaDoador" class="confirm_senhaD" required><br><br>
-    dhsdi: <input type="button" class="jk">
-    </form> 
     
     
 </body>
