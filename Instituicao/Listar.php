dffd8ca3be1507a9e4c5cab93ac2b4d85c14eee9
DomeniqueJoli <83772287+DomeniqueJoli@users.noreply.github.com>
2025-05-31 20:50:23 -0400

    Fazendo o listar de inst

---
+++ Instituicao/Listar.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Instituições Parceiras</title>
+    <link rel="stylesheet" href="styleList.css">
+</head>
+<body>
+
+    <div class="topnav">
+        <img src="..\Images/logo.png" alt="Logo" class="logo">
+        <a href="#">Home</a>
+        <a href="#">Adicionar Ação</a>
+        <a href="#">Doadores</a>
+        <a href="#">Instituições</a>
+        <a href="#">Ações Criadas</a>
+        <a href="#">Perfil</a>
+    </div>
+
+    <main>
+        <h1>Instituições Parceiras</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Logo</th>
+                    <th>Nome</th>
+                    <th>Tipo</th>
+                    <th>Área de Atuação</th>
+                    <th>Estado</th>
+                    <th>Cidade</th>
+                    <th>Contato</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <input type="file" class="inst-img-input">
+                    </td>
+                    <td><input type="text" class="input"></td>
+                    <td><input type="text" class="input"></td>
+                    <td><input type="text" class="input"></td>
+                    <td><input type="text" class="input"></td>
+                    <td><input type="text" class="input"></td>
+                    <td><input type="text" class="input"></td>
+                </tr>
+                <td><button class="ver-mais">Ver mais</button></td>
+
+            </tbody>
+        </table>
+    </main>
+
+</body>
+</html>
