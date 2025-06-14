3f499785ea9de497cce11519a4645264bf560299
DomeniqueJoli <83772287+DomeniqueJoli@users.noreply.github.com>
2025-06-14 19:55:08 -0400

    colocando animação no home pra não trocar todo o design

--- HomeInst.php
+++ HomeInst.php
@@ -1,47 +1,47 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ArmárioSolidário</title>
-    <link rel="icon" href="Images/logo.png" type="image/png">
-    <link rel="stylesheet" href="styleHomeIns.css">
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Armário Solidário</title>
+  <link rel="icon" href="Images/logo.png" type="image/png" />
+  <link rel="stylesheet" href="styleHom.css" />
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
- 
 
-<div class="topnav">    
-<img src="Images/logo.png" alt="" class="logo"> 
-
+  <div class="topnav">    
+    <img src="Images/logo.png" alt="logo" class="logo"> 
     <a href="HomeInst.php">Home</a>    
-    <a href="Acoes\Cadastro.php">Adicionar Ação</a>
-    <a href="Doador\Listar.php">Doadores</a>
-    <a href="Instituicao\Listar.php">Instituições</a>
-    <a href="Acoes\Listar.php">Ações Criadas</a>
-    <a href="Instituicao\Perfil.php">Perfil</a>
-</div>
+    <a href="Acoes/Cadastro.php">Adicionar Ação</a>
+    <a href="Doador/Listar.php">Doadores</a>
+    <a href="Instituicao/Listar.php">Instituições</a>
+    <a href="Acoes/Listar.php">Ações Criadas</a>
+    <a href="Instituicao/Perfil.php">Perfil</a>
+  </div>
 
-<div class="">
-<h6>Pequenos Gestos,</h6>
-<h1>Grandes Impactos</h1>
-</div>
+  <div class="main-content fade-in-down">
+    <div class="typing-wrapper">
+        <h6 class="typing typing-h6">Pequenos Gestos,</h6>
+        <h1 class="typing typing-h1">Grandes Impactos</h1>
+    </div>
 
-<div>
-<h3>Sobre a iniciativa Armário solidário</h3>
-<p>
-O Armário Solidário é uma iniciativa social que tem como missão transformar peças de roupa em bons gestos. Atuamos como uma ponte entre pessoas dispostas a doar e instituições que acolhem <br>
-quem mais precisa, promovendo a solidariedade de forma prática e acessível. Acreditamos que roupas podem aquecer o corpo, mas também são capazes de transmitir dignidade, autoestima e carinho.  <br>
-<br>
 
-Nosso propósito é dar um novo destino a roupas, calçados e acessórios que estão em bom estado, mas sem uso. Conectamos esses itens a crianças, jovens, adultos e idosos em situação de  <br>
-vulnerabilidade, por meio de parcerias com instituições sérias e comprometidas. Cada doação é distribuída com responsabilidade, sempre respeitando a individualidade e a necessidade de quem recebe.  <br>
 
-<br>
-Mais do que doar, o Armário Solidário busca criar uma cultura de empatia, reaproveitamento e consciência social. Queremos inspirar pessoas a olhar com mais atenção para o que têm, entender o valor da  <br>
- partilha e fazer parte de uma corrente do bem. Porque pequenos gestos, quando movidos pelo amor ao próximo, geram grandes impactos.
-</p>
-
-</div>
+    <section class="sobre fade-in-up">
+      <h3>Sobre a iniciativa Armário Solidário</h3>
+      <p>
+        O Armário Solidário é uma iniciativa social que tem como missão transformar peças de roupa em bons gestos. Atuamos como uma ponte entre pessoas dispostas a doar e instituições que acolhem
+        quem mais precisa, promovendo a solidariedade de forma prática e acessível. Acreditamos que roupas podem aquecer o corpo, mas também são capazes de transmitir dignidade, autoestima e carinho.
+        <br><br>
+        Nosso propósito é dar um novo destino a roupas, calçados e acessórios que estão em bom estado, mas sem uso. Conectamos esses itens a crianças, jovens, adultos e idosos em situação de
+        vulnerabilidade, por meio de parcerias com instituições sérias e comprometidas.
+        <br><br>
+        Mais do que doar, o Armário Solidário busca criar uma cultura de empatia, reaproveitamento e consciência social. Queremos inspirar pessoas a olhar com mais atenção para o que têm, entender o valor da
+        partilha e fazer parte de uma corrente do bem. Porque pequenos gestos, quando movidos pelo amor ao próximo, geram grandes impactos.
+      </p>
+    </section>
+  </div>
 
 </body>
 </html>
